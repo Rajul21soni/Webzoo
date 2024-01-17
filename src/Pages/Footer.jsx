58c4f3927346610439393dc691718dca8b07760d
AnujWebDev <anujyash10gmail.com>
2024-01-17 16:16:12 +0530

    changes in footer section

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,34 +1,43 @@
-import React from 'react'
-import { FaFacebook, FaTwitter, FaInstagram, FaGithub  } from "react-icons/fa";
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <>
-        <div className='grid lg:grid-cols-3 mt-10  mx-auto'>
-            <div className='mt-8'>
-                <img className="h-48 s-48" src="https://www.torquevision.ai/assets/images/torqvision.png" alt="logo" />
-                <h1 className='uppercase text-red-500'>
-                    Determined AI Expert
-                </h1>
-            </div>
-            <div className='mt-8'>
-                <h1 className='text-2xl mb-4'>Quick Links</h1>
-                <p className='ml-2 mt-2'>About</p>
-                <p className='ml-2 mt-2'>Work</p>
-                <p className='ml-2 mt-2'>Contact</p>
-            </div>
-            <div className='mt-8' >
-                <h1 className='text-2xl'>Social Media</h1>
-                <div className='flex mt-4 ml-2 text-2xl'>
-                <FaFacebook className='mr-4'/>
-                <FaTwitter className='mr-4'/>
-                <FaGithub  className='mr-4'/>
-                <FaInstagram  className='mr-4'/>
-                </div>
-            </div>
+    <footer className=" text-white py-12 mt-10 border-t-2">
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8">
+        {/* Logo and About */}
+        <div className="flex flex-col items-center justify-center mt-8 lg:mt-0">
+          <img className="h-28 w-28 mb-4" src="https://www.torquevision.ai/assets/images/torqvision.png" alt="logo" />
+          <h1 className="uppercase text-center text-primary  text-lg lg:text-xl mb-4">Determined AI Expert</h1>
         </div>
-    </>
-  )
-}
 
-export default Footer
+        {/* Quick Links */}
+        <div className="text-center lg:text-left">
+          <h1 data-aos="fade-up" data-aos-duration="500"    className="text-lg lg:text-xl mb-4">Quick Links</h1>
+          <p className="mb-2">
+            <a href="#" data-aos="fade-left" data-aos-duration="500" className="text-gray-400 text-center hover:text-white">About</a>
+          </p>
+          <p className="mb-2">
+            <a href="#" data-aos="fade-left" data-aos-duration="1000" className="text-gray-400 hover:text-white">Work</a>
+          </p>
+          <p className="mb-2">
+            <a href="#" data-aos="fade-left" data-aos-duration="1500" className="text-gray-400 hover:text-white">Contact</a>
+          </p>
+        </div>
+
+        {/* Social Media */}
+        <div className="text-center lg:text-left">
+          <h1 data-aos="fade-up" data-aos-duration="500" className="text-lg lg:text-xl mb-4">Social Media</h1>
+          <div className="flex justify-center lg:justify-start mt-4">
+            <FaFacebook data-aos="fade-left" data-aos-duration="1000" className="text-2xl  mr-4" />
+            <FaTwitter data-aos="fade-left" data-aos-duration="1000" className="text-2xl mr-4" />
+            <FaGithub data-aos="fade-left" data-aos-duration="1000" className="text-2xl mr-4" />
+            <FaInstagram data-aos="fade-left" data-aos-duration="1000" className="text-2xl" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
